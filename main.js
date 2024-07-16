d0acf51d946fb4cf61ff37793585780fd2b629da
Nur Farah <nuurfaarax15@gmail.com>
2024-07-16 17:18:53 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+let time = document.querySelector('.time');
+let date = document.querySelector('.date');
+
+
+
+setInterval( () => {
+    all();
+})
+
+let all = () => {
+    let today = new Date();
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct', 'Nov', 'Dec'];
+
+    let days = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+    
+    let currentTimes = `${today.getHours() <= 9 ? '0' + today.getHours() : today.getHours()} : ${today.getMinutes() <= 9 ? '0' + today.getMinutes() : today.getMinutes()} : ${today.getSeconds() <= 9 ? '0' + today.getSeconds() : today.getSeconds()}`;
+    
+    let currentDate = `${days[today.getDay()+1]}, ${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()}`;
+    
+    
+    time.innerHTML = currentTimes;
+    date.innerHTML = currentDate;
+}
+
+
+})
+
